5177358a61e3ce42fd52d3d8d6a4203150e85be3
Avery Reich-Norris <areichnorris@gmail.com>
2015-09-30 13:39:30 -0700

    Started my project.

---
+++ UserInterface/UserInterface/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wolrd!"); //This prints out "Hello World!" to console.
+            Console.ReadLine(); //This makes the code human readable.
+        }
+    }
+}
